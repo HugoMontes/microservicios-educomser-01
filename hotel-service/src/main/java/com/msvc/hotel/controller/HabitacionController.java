7cc17c385a2c514991307375a780bef4c6051036
hmontes <hugomontes927@gmail.com>
2026-08-27 11:51:05 -0400

    API Gateway

--- hotel-service/src/main/java/com/msvc/hotel/controller/HabitacionController.java
+++ hotel-service/src/main/java/com/msvc/hotel/controller/HabitacionController.java
@@ -1,4 +1,21 @@
 package com.msvc.hotel.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/habitaciones")
 public class HabitacionController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> listarHabitaciones() {
+        List<String> habitaciones = Arrays.asList("Habitacion 100", "Habitacion 101", "Habitacion 102");
+        return new ResponseEntity<>(habitaciones, HttpStatus.OK);
+    }
 }
